70afc35619620109c3c9dc7b2658e0424262b3d6
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-06 18:16:37 -0400

    Added the utilityFunctions.py file with the polynomial_sframe() function

---
+++ python/from_scratch/utilityFunctions.py
@@ -0,0 +1,32 @@
+import sframe
+
+
+def polynomial_sframe(feature, degree):
+    '''
+    For a given feature vector x, create a new sframe with columns:
+    x, x^2, x^3, ..., x^degree.
+    
+    @param feature (vector):
+      the vector of the feature x
+    @param degree (int):
+      the highest order polynomial to be considered
+
+    @return: poly_sframe (sframe):
+      a new sframe with columns: x, x^2, x^3, ... x^degree
+    '''
+
+    # Initialize
+    poly_sframe = sframe.SFrame()
+    poly_sframe['x'] = feature
+
+    if degree > 1:
+        for power in range(2, degree + 1):
+            name = 'x^' + str(power)
+            poly_sframe[name] = feature.apply(lambda x: x ** power)
+
+    return poly_sframe
+
+# Test
+test_vec = sframe.SArray([1, 2, 3, 4, 5])
+test_sf = polynomial_sframe(test_vec, 5)
+print test_sf
