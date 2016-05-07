5682172930e26836dae2152a03268b6771aa04ab
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-06 23:19:54 -0400

    ridgeRegression.py started

--- python/from_scratch/regression/ridgeRegression.py
+++ python/from_scratch/regression/ridgeRegression.py
@@ -1,2 +1,14 @@
 import sframe
 import numpy as np
+import os, sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             '../'))
+import utilityFunctions as uf
+
+sales = sframe.SFrame('../../../data/kc_house_data.csv')
+sales = sales.sort(['sqft_living', 'price'])
+
+# Split data into train/validation, and test sets
+(train_valid, test) = sales.random_spit(0.9, seed = 1)
+train_valid_shuffled = 
