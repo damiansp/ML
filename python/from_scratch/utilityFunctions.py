53e5ecf3bede015df244305be3474772dd9e06a9
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-09 22:17:42 -0400

    moved get_numpy_data and get_rss functions out of multipleRegression.py and into utilityFunctions.py

--- python/from_scratch/utilityFunctions.py
+++ python/from_scratch/utilityFunctions.py
@@ -1,5 +1,50 @@
 import sframe
 
+def get_numpy_data(data_sframe, features, Y, verbose = False):
+    '''                                                                       
+    Convert an SFrame into a numpy matrix and response variable (Y) to a
+    numpy array. 
+    @param data_sframe (SFrame):                                               
+       the input data in SFrame format                                         
+    @param features (1-D array of stings):                                     
+       names of features to be included                                        
+    @param Y (string):                                                         
+       name of column to be treated as response variable                       
+    @param verbose (bool):                                                     
+       if True prints first few rows of features (as sframe)                   
+    @return (model_matrix, Y_array)                                            
+       model matrix as an numpy matrix;                                        
+       Y (response variable) as a numpy array                                  
+    '''
+    data_sframe['intercept'] = 1 # bias or intercept term
+    features = ['intercept'] + features
+    features_sframe = data_sframe[features]
+
+    if verbose:
+        print features_sframe.head()
+
+    # convert features_sframe to numpy matrix
+    model_matrix = features_sframe.to_numpy()
+
+    # convert response to np array
+    Y_array = data_sframe[Y].to_numpy()
+
+    return (model_matrix, Y_array)
+
+
+def get_rss(predictions, Y):
+    '''                                                                        
+    @param predictions (1-D array):                                            
+       the predicted values of the model being tested                          
+    @param Y (1-D array):                                                      
+       the true (target) values of the response variable being predicted       
+    @return rss (float):                                                       
+       the residual sum of squares (rss)                                       
+    '''
+    error = predictions - Y
+    rss = sum(error ** 2)
+    return rss
+
 
 def polynomial_sframe(feature, degree):
     '''
