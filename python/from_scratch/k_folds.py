741509348563ec2ac2fb09874f3b39ac51eac258
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-13 18:07:00 -0400

    Initialized new k_folds() in k_folds.py to generalize to any model (incomplete)

--- python/from_scratch/k_folds.py
+++ python/from_scratch/k_folds.py
@@ -62,11 +62,62 @@ def k_folds_regression(k,
                                               eta = eta,
                                               tolerance = tolerance,
                                               max_iterations = max_iterations,
-                                              # l1_penalty = 0,
                                               l2_penalty = l2_penalty)
 
         # Test model on the validation group
         predictions = predict(X_validation, weights)
+        error = predictions - Y_validation
+        total_rss += sum(error ** 2)
+
+    return total_rss / k
+
+
+
+# TO DO: Generalize k_folds to work with any model
+# Requires that all models output predictions (GD and CD currently output
+# optimized weights only)
+def k_folds(k, model, X, Y):
+    '''
+    Perform k-folds cross validation on a model using model matrix X and 
+    response variable Y
+
+    @param k (int): 
+      number of folds (k = n_row(X) is optimal but slowest)
+    @param model (dict):
+      a dictionary with model: name_of_model, and parameters to model passed in
+    @param X (matrix):
+      the model matrix
+    @param Y (vector):
+      the response variable values
+
+    @return total_rss / k (float):
+    the mean rss over all folds
+    '''
+
+    n = len(X)
+    fold_size = n / k
+    total_rss = 0 # init
+    
+    for fold in xrange(k):
+        fold_start = fold * fold_size
+        
+        # hold out a validation set                                            
+        X_validation = X[fold_start:(fold_start + fold_size)]
+        Y_validation = Y[fold_start:(fold_start + fold_size)]
+
+        # the test set is then all data except the validation set              
+        # data before the validation set:                                      
+        train_pre = X[0:fold_start, :]
+        Y_pre = Y[0:fold_start]
+        # and after it:                                                        
+        train_post = X[(fold_start + fold_size):n, :]
+        Y_post = Y[(fold_start + fold_size):n]
+        # merge pre and post:                                                  
+        X_train = np.vstack((train_pre, train_post))
+        Y_train = np.hstack((Y_pre, Y_post))
+
+        # Test model on the validation group
+        predictions = 
         error = predictions - Y_validation
         total_rss += sum(error ** 2)
 
