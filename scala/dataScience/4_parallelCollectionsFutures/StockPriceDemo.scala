cf1e7a3a98b8308d190f76968e523aac7ec867a7
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-19 08:53:35 -0700

    init Stock demo

---
+++ scala/dataScience/4_parallelCollectionsFutures/StockPriceDemo.scala
@@ -0,0 +1,9 @@
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io._
+import scala.util._
+import scala.xml.XML
+
+object StockPriceDemo extends App {
+	
+}
