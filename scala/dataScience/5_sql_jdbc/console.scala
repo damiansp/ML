feb22813ba700569d19d2130eb0d9e31143eee01
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-28 22:33:05 -0400

    console++, +Gender

--- scala/dataScience/5_sql_jdbc/console.scala
+++ scala/dataScience/5_sql_jdbc/console.scala
@@ -13,8 +13,8 @@ CREATE TABLE pysicists (
 	name VARCHAR(32) NOT NULL);"""
 
 // Insert
-val statement = connection.prepareStatement("""
-	INSERT INTO physicists (name) VALUES ('Isaac Newton')""")
+val statement = connection.prepareStatement(
+  """INSERT INTO physicists (name) VALUES ('Isaac Newton')""")
 
 val physicistNames = List("Marie Curie", "Albert Einstein", "Paul Dirac")
 val statement2 = connection.prepareStatement("""
@@ -26,4 +26,24 @@ physicistNames.foreach { name =>
 	statement2.addBatch()
 }
 statement2.executeBatch
+
+
+
+// Reading data
+val statement3 = connection.prepareStatement("""SELECT name FROM physicists""")
+val results3 = statement3.executeQuery()
+results3.next // advance cursor to first record
+results3.getString("name") // "Isaac Newton"
+results3.getString(1) // first positional arg ("Isaac Newton")
+results3.next
+results3.getString("name") // "Richard Feynman"
+while (results3.nex) { println(results3.getString("name")) }
+
+// Warning about null fields 
+rs.getInt("field") // 0
+rs.wasNull         // True (Null values return as 0, 0.0, "", etc)
+
+results3.close
+statement3.close
+connection.close
 
