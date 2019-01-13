a1b4637d5627bcbcbc5bb3c1f39f6616a4011a58
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-13 15:57:35 -0500

    add HttpURLConnectionExample

---
+++ java/javaForDataScience/src/main/java/J4DS/02_dataAquisition/HttpURLConnectionExample.java
@@ -0,0 +1,43 @@
+package httpurlconnectionexample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import static java.lang.System.out;
+
+
+public class HttpURLConnectionExample {
+  public static void main(String[] args) {
+    try {
+      URL url = new URL("https://en.wikipedia.org/wiki/Data_science");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      
+      conn.setRequestMethod("GET");
+      conn.connect();
+      out.println("Repsonse Code: " + conn.getResponseCode());
+      out.println("Content Type: " + conn.getContentType());
+      out.println("Content Length: " + conn.getContentLength());
+      
+      InputStreamReader isr = new InputStreamReader(
+        (InputStream) conn.getContent());
+      BufferedReader br = new BufferedReader(isr);
+      StringBuilder buffer = new StringBuilder();
+      String line;
+
+      do {
+        line = br.readLine();
+        buffer.append(line + "\n");
+      } while (line != null);
+      out.println(buffer.toString());
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (IOException e) { e.printStackTrace(); }
+  }
+}
+
+
+  
