b8db213996b088e105f2cc3dce17b00a00a9b2b6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-28 07:49:37 -0700

    fin addends

--- python/bayes/05_odds_and_addends/addends.py
+++ python/bayes/05_odds_and_addends/addends.py
@@ -3,7 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 
 sys.path.append('..')
-from thinkbayes import Pmf, SampleSum
+from thinkbayes import MakeMixture, Pmf, SampleSum
 
 
 class Die(Pmf):
@@ -27,4 +27,68 @@ exact = sorted(three_exact.Items())
 h_ex, p_ex = [a[0] for a in exact], [a[1] for a in exact]
 plt.plot(h_ex, p_ex, 'r-', label='exact')
 plt.legend()
+plt.show()
+
+
+# Maxima
+def RandomMax(dists):
+    total = max(dist.Random() for dist in dists)
+    return total
+
+
+def SampleMax(dists, n):
+    pmf = MakePmfFromList(RandomMax(dists) for _ in range(n))
+    return pmf
+
+
+def PmfMax(pmf1, pmf2):
+    res = Pmf()
+    for v1, p1 in pmf1.Items():
+        for v2, p2 in pmf2.Items():
+            res.Incr(max(v1, v2), p1 * p2)
+    return res
+
+# Far more efficient:
+# In class Cdf:
+#def Max(self, k):
+#    cdf = self.Copy()
+#    cdf.ps = [p ** k for p in cdf.ps]
+#    return cdf
+
+best_attr_cdf = three_exact.Max(6)
+best_attr_pmf = best_attr_cdf.MakePmf()
+max_items = sorted(best_attr_pmf.Items())
+h_max, p_max = [a[0] for a in max_items], [a[1] for a in max_items]
+plt.plot(h_max, p_max)
+plt.xlabel('Max value')
+plt.ylabel('Prob')
+plt.show()
+
+
+# Randomly choose 1 die from the set and roll. What is P(outcome)?
+# 2 dice example: 1 6-sided, 1 8-sided
+d6 = Die(6)
+d8 = Die(8)
+mix = Pmf()
+for die in [d6, d8]:
+    for outcome, prob in die.Items():
+        mix.Incr(outcome, prob)
+mix.Normalize()
+
+
+# More complicated set of dice
+pmf_dice = Pmf()
+pmf_dice.Set(Die(4), 2) # 2 4-sided dice
+pmf_dice.Set(Die(6), 3) # 3 6-sided, etc
+pmf_dice.Set(Die(8), 2)
+pmf_dice.Set(Die(12), 1)
+pmf_dice.Set(Die(20), 1)
+pmf_dice.Normalize()
+
+mix = MakeMixture(pmf_dice)
+mix_items = sorted(mix.Items())
+h_mix, p_mix = [a[0] for a in mix_items], [a[1] for a in mix_items]
+plt.plot(h_mix, p_mix)
+plt.xlabel('Outcome')
+plt.ylabel('Prob')
 plt.show()
