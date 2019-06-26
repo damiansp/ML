a8c4d0e8a442beb346c875a233308cca22dd4d14
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-06-26 09:34:49 -0700

    Complete bruins.py

--- python/bayes/07_prediction/bruins.py
+++ python/bayes/07_prediction/bruins.py
@@ -5,7 +5,8 @@ import numpy as np
 import scipy
 
 sys.path.append('..')
-from thinkbayes import MakeGaussianPmf, MakeMixture, Pmf, Suite
+from thinkbayes import (
+    MakeExponentialPmf, MakeGaussianPmf, MakeMixture, Pmf, PmfProbLess, Suite)
 
 
 MAX_LIKELY_GOALS = 10
@@ -68,3 +69,29 @@ suite2 = Hockey('canucks')
 suite2.UpdateSet([1, 3, 1, 0])
 lam = 3.4
 goal_dist = MakePoissonPmf(lam, MAX_LIKELY_GOALS)
+
+# Prob of winning
+goal_dist1 = MakeGoalPmf(suite1)
+goal_dist2 = MakeGoalPmf(suite2)
+diff = goal_dist1 - goal_dist2
+p_win = diff.ProbGreater(0)
+p_tie = diff.Prob(0)
+print(f'P(win): {p_win:.4f}\nP(tie): {p_tie:.4f}')
+
+# Sudden death
+lam = 3.4
+time_dist = MakeExponentialPmf(lam, high=2, n=101)
+
+def MakeGoalTimePmf(suite):
+    metapmf = Pmf()
+    for lam, prob in suite.Items():
+        pmf = MakeExponentialPmf(lam, high=2, n=2001)
+        metapmf.Set(pmf, prob)
+    mix = MakeMixture(metapmf)
+    return mix
+
+time_dist1 = MakeGoalTimePmf(suite1)
+time_dist2 = MakeGoalTimePmf(suite2)
+p_overtime = PmfProbLess(time_dist1, time_dist2)
+p_win_revised = p_win + p_tie*p_overtime
+print(f'P(win): {p_win_revised:.4fn}')
