03ef742e1e6babf451f742b17cec8bf2f1455fed
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-06-25 12:02:44 -0700

    basic img clf++

--- python/tf/tutorial/beginner/basic_img_classification.py
+++ python/tf/tutorial/beginner/basic_img_classification.py
@@ -10,14 +10,24 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 DATA = '/Users/damiansp/Learning/ML/data'
 LABELS = [
-    'T-shirt/Top', 'Pants', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker',
-    'Bag', 'Ankle boot']
+    'T-shirt/Top', 'Pants', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt',
+    'Sneaker', 'Bag', 'Ankle boot']
+EPOCHS = 10
 
 
 def main():
-    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    (X_train, y_train), (X_test, y_test) = (
+        tf.keras.datasets.fashion_mnist.load_data())
     explore(X_train, y_train, X_test, y_test)
     X_train, X_test = preprocess(X_train, X_test, y_train)
+    mod = init_mod()
+    mod.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
+    mod.fit(X_train, y_train, epochs=EPOCHS)
+    test_loss, test_acc = mod.evaluate(X_test, y_test, verbose=2)
+    print(f'Test acc: {test_acc:.4f}')
 
 
 def explore(X_train, y_train, X_test, y_test):
@@ -52,6 +62,14 @@ def plot_sample(X, y):
         plt.imshow(X[i], cmap=plt.cm.binary)
         plt.xlabel(LABELS[y[i]])
     plt.show()
+
+
+def init_mod():
+    mod = tf.keras.Sequential([
+        tf.keras.layers.Flattern(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(len(LABELS))])
+    return mod
                    
 
 if __name__ == '__main__':
