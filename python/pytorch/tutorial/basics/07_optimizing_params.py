b6562788408eabd087673e7b8aa3d7ca3a922b92
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-08 20:06:09 -0700

    optim. params++

--- python/pytorch/tutorial/basics/07_optimizing_params.py
+++ python/pytorch/tutorial/basics/07_optimizing_params.py
@@ -7,18 +7,23 @@ from torchvision.transforms import ToTensor
 
 ETA = 1e-3
 BATCH = 64
-EPOCHS = 5
+EPOCHS = 10
 
 
 def main():
-    training_data, test = load_data()
+    training_data, test_data = load_data()
     train_dataloader = DataLoader(training_data, batch_size=64)
     test_dataloader = DataLoader(test_data, batch_size=64)
     mod = NeuralNetwork()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(mod.parameters(), lr=ETA)
-    
+    for epoch in range(EPOCHS):
+        print(f'Epoch {epoch + 1}\n------------------------------------------')
+        train(train_dataloader, mod, loss_fn, optimizer)
+        test(test_dataloader, mod, loss_fn)
+    print('Done!')
 
+    
 def load_data():
     return [
         datasets.FashionMNIST(
@@ -26,8 +31,7 @@ def load_data():
             train=tf,
             download=True,
             transform=ToTensor())
-        for tf in [True, False]
-
+        for tf in [True, False]]
 
 
 class NeuralNetwork(nn.Module):
@@ -46,3 +50,43 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+
+
+
+def train(dataloader, mod, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # Set mod to training - imoprtant for batch norm and dropout;
+    # unnecessary here
+    mod.train()
+    for batch, (X, y) in enumerate(dataloader):
+        pred = mod(X)
+        loss = loss_fn(pred, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f'Loss: {loss:>7f} [{current:>5d}/{size:>5d}]')
+
+
+def test(dataloader, mod, loss_fn):
+    # Also unnecessary here:
+    mod.eval()
+    size = len(dataloader.dataset)
+    n_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    # Eval mod with no_grad prevents gradients from being computed
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = mod(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        test_loss /= n_batches
+        correct /= size
+        print(
+            f'Test err:\n Acc: {100 * correct:>0.1f}%; '
+            f'mean loss: {test_loss:>8f}\n')
+        
+        
+if __name__ == '__main__':
+    main()
