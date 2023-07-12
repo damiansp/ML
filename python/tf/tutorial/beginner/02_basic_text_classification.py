70edbcc9bb2463b3fc58b1dfc2a8f55733251525
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-12 13:45:02 -0700

    basic txt clf++

--- python/tf/tutorial/beginner/02_basic_text_classification.py
+++ python/tf/tutorial/beginner/02_basic_text_classification.py
@@ -11,8 +11,12 @@ from tensorflow.keras import layers, losses
 print(tf.__version__)
 
 
+BATCH = 32
+SEED = 42
+
+
 def main():
-    datset = download_data()
+    raw_dataset = download_data()
 
 
 def download_data():
@@ -26,3 +30,24 @@ def download_data():
     sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
     with open(sample_file) as f:
         print(f.read())
+    remove_dir = os.path.join(train_dir, 'unsup')
+    shutil.rmtree(remove_dir)
+    raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+        'aclImdb/train',
+        batch_size=BATCH,
+        validation_split=0.2,
+        subset='training',
+        seed=SEED)
+    for text_batch, label_batch in raw_train_ds.take(1):
+        for i in range(3):
+            print('Review:', text_batch.numpy()[i])
+            print('Label:', label_batch.numpy()[i]) # 0: neg; 1: pos
+    raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+        'aclImdb/train',
+        batch_size=BATCH,
+        validation_split=0.2,
+        subset='validation',
+        seed=SEED)
+    raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+        'aclImdb/test', batch_size=BATCH)
+    return raw_train_ds, raw_val_ds, raw_test_ds
