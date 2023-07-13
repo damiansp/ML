5cb13a17899120412ad4739c27610d1ee7e1faff
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-13 10:42:27 -0700

    basic txt clf++

--- python/tf/tutorial/beginner/02_basic_text_classification.py
+++ python/tf/tutorial/beginner/02_basic_text_classification.py
@@ -13,10 +13,13 @@ print(tf.__version__)
 
 BATCH = 32
 SEED = 42
+MAX_FEATURES = 10_000
+SEQ_LEN = 250
 
 
 def main():
     raw_dataset = download_data()
+    dataset = prep_data(raw_dataset)
 
 
 def download_data():
@@ -51,3 +54,29 @@ def download_data():
     raw_test_ds = tf.keras.utils.text_dataset_from_directory(
         'aclImdb/test', batch_size=BATCH)
     return raw_train_ds, raw_val_ds, raw_test_ds
+
+
+def prep_data(raw_dataset):
+    raw_train_ds, raw_val_ds, raw_test_ds = raw_dataset
+    vectorize_layer = layers.TextVectorization(
+        strandardize=standardize_text,
+        max_tokens=MAX_FEATURES,
+        output_mode='int',
+        output_sequence_length=SEQ_LEN)
+    train_text = raw_train_ds.map(lambda x, y: x)
+    vectorize_layer.adapt(train_text)
+    text_batch, label_batch = next(iter(raw_train_ds))
+    first_review, first_label = text_batch[0], label_batch[0]
+    print('Review:', first_review)
+    print('Label:', raw_train_ds.class_names[first_label])
+    print('Vectorized:', vectorize_text(first_review, first_label))
+    print('1287 --> ', vectorize_layer.get_vocabulary()[1287])
+    print(' 313 --> ', vectorize_layer.get_vocabulary()[313])
+    print('Vocab size:', len(vectorize_layer.get_vocabulary()))
+
+
+def standardize_text(data):
+    lower = tf.strings.lower(data)
+    no_html = tf.strings.regex_replace(lower, '<br />', ' ')
+    return tf.strings.regex_replace(
+        no_html, '[%s]' % re.escape(string.punctuation), '')
