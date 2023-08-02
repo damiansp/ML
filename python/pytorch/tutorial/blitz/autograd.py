4e5f55ca58e2b1fbf32e2a3a45671e6b2d38cbd5
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-08-02 11:15:41 -0700

    autograd++

--- python/pytorch/tutorial/blitz/autograd.py
+++ python/pytorch/tutorial/blitz/autograd.py
@@ -14,6 +14,18 @@ def main():
     loss.backward()    # backward pass
     optim = torch.optim.SGD(mod.parameters(), lr=ETA, momentum=MOMENTUM)
     optim.step()       # grad desc
+    differentiate()
+
+
+def differentiate():
+    a = torch.tensor([2., 3.], requires_grad=True)
+    b = torch.tensor([6., 4.], requires_grad=True)
+    Q = 3 * a**3 - b**2
+    external_grad = torch.tensor(1., 1.])
+    Q.backward(gradient=external_grad)
+    print(a.grad == 9 * a**2)  # True
+    print(b.grad == -2 * b)    # True
+    
     
 
 
