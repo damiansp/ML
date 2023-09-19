e8b17f5fb5448b16046757b0aebea2807bdf4b0a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-19 08:46:39 -0700

    img clf++

--- python/pytorch/tutorial/blitz/img_classifier.py
+++ python/pytorch/tutorial/blitz/img_classifier.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
@@ -9,11 +12,17 @@ BATCH = 4
 CLASSES = (
     'plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship',
     'truck')
+ETA = 0.001
+MOMENTUM = 0.9
+EPOCHS = 2
+MOD_PATH = './cifar_net.pth'
 
 
 def main():
     train_loader, test_loader = get_data_loaders()
     show_random_images(train_loader)
+    net = Net()
+    net = train(train_loader, net)
 
 
 def get_data_loaders():
@@ -45,5 +54,48 @@ def show_img(img):
     plt.show()
 
 
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, len(CLASSES))
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def train(train_loader, net):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=ETA, momentum=MOMENTUM)
+    for epoch in range(EPOCHS):
+        running_loss = 0.
+        for i, data in enumerate(train_loader, 0): # 0 is redundant/default
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            losss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 2_000 = 1999:
+                print(
+                    f'[{epoch + 1}, {i + 1:5d}] '
+                    f'loss: {running_loss / 2000:.3f}')
+                running_loss = 0.
+    print('Training complete')
+    torch.save(net.state_dict(), MOD_PATH)
+    print('Model saved to', MOD_PATH)
+    return net
+    
 if __name__ == '__main__':
     main()
